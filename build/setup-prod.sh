12a5e1ce6b26dba134f3cc7f2a7eece59d608361
Ashish Kumar Gupta <ashish@business2sell.com.au>
2020-09-24 15:31:44 +0530

    Allow permitted to all pre-authorized users

--- build/setup-prod.sh
+++ build/setup-prod.sh
@@ -3,6 +3,6 @@ echo "Setting up Production Connection..."
 mkdir keys
 echo $SFDC_SERVER_KEY | base64 -di > keys/server.key
 echo $SFDC_SERVER_KEY
-# Authenticate to salesforce
+# Authenticate to salesforce  
 echo "Authenticating..."
 sfdx force:auth:jwt:grant --clientid $SFDC_PROD_CLIENTID --jwtkeyfile keys/server.key --username $SFDC_PROD_USER --setdefaultdevhubusername -a DevHub
